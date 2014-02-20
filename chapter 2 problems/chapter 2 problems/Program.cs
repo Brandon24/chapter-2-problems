9ad87cbe955b998b934209bd5b9a8cf2c4f7fc2f
Brandon24 <brandonreinert24@gmail.com>
2014-02-20 09:59:04 -0500

    Day 1

---
+++ chapter 2 problems/chapter 2 problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chapter_2_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 15, width = 25, area;
+            area = length * width;
+            Console.WriteLine("The floor space is {0} ", area );
+        }
+    }
+}
